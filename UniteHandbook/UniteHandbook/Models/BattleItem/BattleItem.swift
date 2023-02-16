59be4462dc58b6f5a4fc97025b230d0350da9ac1
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-02-15 19:53:20 -0700

    Create BattleItem model

---
+++ UniteHandbook/UniteHandbook/Models/BattleItem/BattleItem.swift
@@ -0,0 +1,44 @@
+//
+//  BattleItem.swift
+//  UniteHandbook
+//
+//  Created by Jayme Rutkoski on 2/15/23.
+//
+
+import Foundation
+
+public class BattleItem : BaseListDiffable, Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "Name"
+        case cooldown = "BonusStats"
+        case desc = "Desc"
+    }
+    
+    public var name: String = ""
+    public var cooldown: Double = 0
+    public var desc: String = ""
+    
+    public override init() { }
+
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.cooldown = try container.decode(Double.self, forKey: .cooldown)
+        self.desc = try container.decode(String.self, forKey: .desc)
+    }
+    
+    static func loadJson(filename fileName: String) -> BattleItem {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(BattleItem.self, from: data)
+                return jsonData
+            } catch {
+                print("error:\(error)")
+            }
+        }
+        return BattleItem()
+    }
+}
